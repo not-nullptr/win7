9d98fc40de93a6469e586343c7d1917f0a605478
not-nullptr <needhelpwithrift@gmail.com>
2023-10-24 22:02:52 +0100

    fix live

--- src/windows/live/pages/Home.tsx
+++ src/windows/live/pages/Home.tsx
@@ -82,7 +82,7 @@ function Home({ win }: { win?: Window }) {
 		};
 	}, [win]);
 	const [search, setSearch] = useState("");
-	const { sendJsonMessage } = useWebSocket("ws://localhost:4000", {
+	const { sendJsonMessage } = useWebSocket("wss://win7api.nota-robot.com", {
 		onOpen(e) {
 			const username = localStorage.getItem("username");
 			const statusMessage = localStorage.getItem("statusMessage");
